0fd8c3dea5b5695a61f7ebb6f0f415e553b652ea
Fedja Jovanovic <fedja2000@bih.net.ba>
2022-03-19 23:55:58 +0100

    DATA BINDING SA EDIT USERIMA

--- intralinkdemo/EditUserWindow.xaml.cs
+++ intralinkdemo/EditUserWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
 
 namespace intralinkdemo
 {
@@ -27,6 +29,59 @@ namespace intralinkdemo
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();   
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            UserNameTextBoxEDIT.Clear();
+            UserAddPasswordFieldEDIT.Clear();
+            Name_User_Textbox.Clear();
+            Surname_Textbox.Clear();
+            Email_textbox.Clear();
+            Position_Textbox.Clear();
+            Salary_textbox.Clear();
+
+
+
+
+
+
+        }
+
+        private void UserIDEDIT_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+            OleDbConnection con_admin_edit = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users_final.mdb");
+            con_admin_edit.Open();
+
+
+
+            if(UserIDEDIT.Text != "") { 
+
+            OleDbCommand cmd = new OleDbCommand("SELECT [Username],[Password],[Name_User] FROM tbl_users_final WHERE [ID]="+int.Parse(UserIDEDIT.Text)+"", con_admin_edit);
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+
+
+                UserNameTextBoxEDIT.Text = reader.GetValue(0).ToString();
+                UserAddPasswordFieldEDIT.Text = reader.GetValue(1).ToString();
+                Name_User_Textbox.Text = reader.GetValue(2).ToString();
+
+
+
+
+            }
+
+
+
+            con_admin_edit.Close();
+
+            }
+
+
         }
     }
 }
--- intralinkdemo/UserRemoveWindow.xaml.cs
+++ intralinkdemo/UserRemoveWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
 
 namespace intralinkdemo
 {
@@ -22,6 +24,102 @@ namespace intralinkdemo
         public UserRemoveWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Close();
+
+
+
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            OleDbConnection connection_admin_view = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users_final.mdb");
+            OleDbCommand command_admin_view = new OleDbCommand();
+            OleDbDataAdapter data_adapter_admin_view = new OleDbDataAdapter();
+
+            try
+            {
+
+                connection_admin_view.Open();
+                string Query_view = "SELECT [ID],[Username],[Password],[Name_User],[Surname_User],[Email],[Position],[Pay] FROM tbl_users_final";
+                OleDbCommand cmd_admin_view = new OleDbCommand(Query_view, connection_admin_view);
+                cmd_admin_view.ExecuteNonQuery();
+
+
+                OleDbDataAdapter admin_data_adapter_view = new OleDbDataAdapter(cmd_admin_view);
+                DataTable dt = new DataTable("tbl_users_final");
+                admin_data_adapter_view.Fill(dt);
+                data_grid_remove.ItemsSource = dt.DefaultView;
+                admin_data_adapter_view.Update(dt);
+
+                data_grid_remove.Visibility = Visibility.Visible;
+                connection_admin_view.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            OleDbConnection connection_admin_delete = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users_final.mdb");
+            OleDbCommand command_admin_delete = new OleDbCommand();
+            OleDbDataAdapter data_adapter_admin_delete = new OleDbDataAdapter();
+
+
+            try
+            {
+
+
+
+                connection_admin_delete.Open();
+                string Query_delete = "  delete from  tbl_users_final where [ID]= " + this.UserIdBox.Text + "  ";
+                OleDbCommand cmd_admin_delete = new OleDbCommand(Query_delete, connection_admin_delete);
+                cmd_admin_delete.ExecuteNonQuery();
+                MessageBox.Show("User Deleted");
+
+
+                connection_admin_delete.Close();
+
+
+
+      
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+
+
         }
     }
 }
