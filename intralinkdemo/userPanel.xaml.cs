37747c0af98b9497a904b787f7af6ad9a5eb7c8e
uljicamer <uljicamer@gmail.com>
2022-03-19 15:13:07 +0100

    LOGIN FORMA SA BLANK USER I ADMIN PANELIMA

--- intralinkdemo/userPanel.xaml.cs
+++ intralinkdemo/userPanel.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace intralinkdemo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for userPanel.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class userPanel : Window
     {
-        public MainWindow()
+        public userPanel()
         {
             InitializeComponent();
-        }
-
-      
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void exitButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
         }
     }
 }
