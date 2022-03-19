716b0ad3e4ba88ed2361ca99198179b477cb4fe7
Fedja Jovanovic <fedja2000@bih.net.ba>
2022-03-19 18:00:08 +0100

    BASIC FUNKCJONALNOST ADMIN PANELA ZA LISTANJE USERA

--- intralinkdemo/adminPanel.xaml.cs
+++ intralinkdemo/adminPanel.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
 
 namespace intralinkdemo
 {
@@ -23,15 +25,61 @@ namespace intralinkdemo
         {
             InitializeComponent();
 
+
+            data_grid_view.Visibility = Visibility.Hidden;
+            
+
+
         }
+
+        OleDbConnection connection_admin_view = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users_final.mdb");
+        OleDbCommand command_admin_view = new OleDbCommand();
+        OleDbDataAdapter data_adapter_admin_view = new OleDbDataAdapter();
+
 
         private void exitButtonAdminPanel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+
+       
+
         private void ExpanderUserButton_Click(object sender, RoutedEventArgs e)
         {
+
+            try
+            {
+
+                connection_admin_view.Open();
+                string Query_view = "SELECT [ID],[Username],[Password],[Name_User],[Surname_User],[Email],[Position],[Pay] FROM tbl_users_final";
+                OleDbCommand cmd_admin_view = new OleDbCommand(Query_view,connection_admin_view);
+                cmd_admin_view.ExecuteNonQuery();
+
+
+                OleDbDataAdapter admin_data_adapter_view = new OleDbDataAdapter(cmd_admin_view);
+                DataTable dt = new DataTable("tbl_users_final");
+                admin_data_adapter_view.Fill(dt);
+                data_grid_view.ItemsSource = dt.DefaultView;
+                admin_data_adapter_view.Update(dt);
+
+                data_grid_view.Visibility = Visibility.Visible;
+
+                connection_admin_view.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+            
 
         }
     }
